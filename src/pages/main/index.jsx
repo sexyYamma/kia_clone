a3d34b71e865c39f0c86ff85e95a01c465be4760
seonghoon07 <chltjdgns1009@gmail.com>
2024-06-29 17:31:31 +0900

    ThirdPage 파일생성

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import * as S from "./layouts/FirstPage/style";
 import Footer from "../../components/Footer";
 import FirstPage from "./layouts/FirstPage";
+import ThirdPage from "./layouts/ThirdPage";
 
 const Main = () => {
   return (
     <>
       <FirstPage />
+      <ThirdPage />
       <Footer />
     </>
   );
---
+++ src/pages/main/layouts/ThirdPage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style'
+
+const ThirdPage = () => {
+  return (
+    <S.Layout>
+      
+    </S.Layout>
+  );
+};
+
+export default ThirdPage;
