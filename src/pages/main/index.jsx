a825d013b0692d4123e16bd11f0a305578afa6ab
seonghoon07 <chltjdgns1009@gmail.com>
2024-06-29 18:36:48 +0900

    뉴스 리스트 페이지 완성

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,12 +3,14 @@ import * as S from "./layouts/FirstPage/style";
 import Footer from "../../components/Footer";
 import FirstPage from "./layouts/FirstPage";
 import ThirdPage from "./layouts/ThirdPage";
+import FourthPage from "./layouts/FourthPage";
 
 const Main = () => {
   return (
     <>
       <FirstPage />
       <ThirdPage />
+      <FourthPage />
       <Footer />
     </>
   );
